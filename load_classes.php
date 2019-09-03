dfab68543716080e26ccd8c23c29a15df202c9c0
Felix Althaus <felix.althaus@undkonsorten.com>
2019-09-03 14:48:57 +0200

    [TASK] Remove composer

---
+++ load_classes.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This file is part of the composer package "undkonsorten/typo3-auto-login" for use with TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+require_once __DIR__ . '/src/Exception/NotAllowedException.php';
+require_once __DIR__ . '/src/Service/AutomaticAuthenticationService.php';
+require_once __DIR__ . '/src/Utility/RegisterServiceUtility.php';
