51d3c4d7183a089808141b66f222587280c35ffd
Elias Häußler <elias@haeussler.dev>
2024-10-05 17:41:01 +0200

    [TASK] Remove unused Rector rule

--- build/cgl/rector.php
+++ build/cgl/rector.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Ssch\TYPO3Rector\Set\Typo3SetList;
 
@@ -11,10 +10,6 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
         getcwd() . '/src',
         getcwd() . '/tests',
-    ]);
-
-    $rectorConfig->skip([
-        AddLiteralSeparatorToNumberRector::class,
     ]);
 
     $rectorConfig->sets([
